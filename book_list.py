4cecd0a2a55bb4ad3489ec45573b1eba550f2382
mujina <a.mujinasawa.sys22@morijyobi.ac.jp>
2023-11-01 14:00:23 +0900

    list

--- book_list.py
+++ book_list.py
@@ -16,16 +16,43 @@ def show_book_list():
         )
         cursor = conn.cursor()
 
-        cursor.execute("SELECT id,title, author, company, ISBN FROM book")  # テーブルと列名を調整
+        cursor.execute("SELECT id, title, author, company, ISBN FROM book")  # テーブルと列名を調整
         books = cursor.fetchall()
 
-        # 本を表示するリストボックスを作成
-        book_listbox = tk.Listbox(book_list_window)
-        book_listbox.pack()
+        # ラベルとエントリウィジェットを含むフレームを作成
+        frame = tk.Frame(book_list_window)
+        frame.pack()
 
+        id_label = tk.Label(frame, text="ID")
+        id_label.grid(row=0, column=0)
+        title_label = tk.Label(frame, text="本のタイトル")
+        title_label.grid(row=0, column=1)
+        author_label = tk.Label(frame, text="著者")
+        author_label.grid(row=0, column=2)
+        company_label = tk.Label(frame, text="出版社")
+        company_label.grid(row=0, column=3)
+        isbn_label = tk.Label(frame, text="ISBN")
+        isbn_label.grid(row=0, column=4)
+
+        row_number = 1
         for book in books:
-            book_info = f"{book[0]},{book[1]},{book[2]},{book[3]},{book[4]}"
-            book_listbox.insert(tk.END, book_info)
+            id_entry = tk.Entry(frame)
+            id_entry.insert(0, book[0])
+            id_entry.grid(row=row_number, column=0)
+            title_entry = tk.Entry(frame)
+            title_entry.insert(0, book[1])
+            title_entry.grid(row=row_number, column=1)
+            author_entry = tk.Entry(frame)
+            author_entry.insert(0, book[2])
+            author_entry.grid(row=row_number, column=2)
+            company_entry = tk.Entry(frame)
+            company_entry.insert(0, book[3])
+            company_entry.grid(row=row_number, column=3)
+            isbn_entry = tk.Entry(frame)
+            isbn_entry.insert(0, book[4])
+            isbn_entry.grid(row=row_number, column=4)
+
+            row_number += 1
 
         cursor.close()
         conn.close()
