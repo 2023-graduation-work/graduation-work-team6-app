52d9cde4d59838b58831b84708048d841b9d9cc6
to <t.higashimoto.sys22@morijyobi.ac.jp>
2023-11-15 14:59:31 +0900

    aaa

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 import tkinter as tk
-import book_list
-import add_book
-import search_book
-import lend_book
-import delete_book
-import check_availability
-import return_book  
+from tkinter import ttk
+import book_list  
+import add_book  
+import search_book  
+import lend_book 
+import delete_book  
+import check_availability  
+
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
 
 root = tk.Tk()
 root.title("図書管理アプリ")
+
+# ウィンドウのサイズ
+window_width = root.winfo_screenwidth() - 100
+window_height = root.winfo_screenheight() - 100
+
+# ウィンドウのサイズを指定
+root.geometry(f"{window_width}x{window_height}")
+
+# 図書管理メニューラベルの作成
+menu_label = tk.Label(root, text="図書管理メニュー", font=('Helvetica', 24))
+menu_label.pack(pady=(50, 20))
+
+# スタイルの設定
+style = ttk.Style()
+style.configure("TButton", padding=15, font=('Helvetica', 16))
 
 def show_book_list():
     book_list.show_book_list()
@@ -19,7 +43,7 @@ def show_add_book():
 def show_search_book():
     search_book.show_search_book()
 
-def show_lend_book():
+def show_lend_book(): 
     lend_book.show_lend_book()
 
 def show_delete_book():
@@ -28,23 +52,29 @@ def show_delete_book():
 def show_check_availability():
     check_availability.show_check_availability()
 
-def show_return_book():
-    return_book.show_return_book()
+# ボタンの作成
+list_books_button = ttk.Button(root, text="本の一覧", command=show_book_list, style="TButton")
+add_book_button = ttk.Button(root, text="本の登録", command=show_add_book, style="TButton")
+search_book_button = ttk.Button(root, text="本の検索", command=show_search_book, style="TButton")
+lend_book_button = ttk.Button(root, text="本の貸出", command=show_lend_book, style="TButton")
+delete_book_button = ttk.Button(root, text="本の削除", command=show_delete_book, style="TButton")
+check_availability_button = ttk.Button(root, text="貸出状況確認", command=show_check_availability, style="TButton")
 
-list_books_button = tk.Button(root, text="本の一覧", command=show_book_list)
-add_book_button = tk.Button(root, text="本の登録", command=show_add_book)
-search_book_button = tk.Button(root, text="本の検索", command=show_search_book)
-lend_book_button = tk.Button(root, text="本の貸出", command=show_lend_book)
-delete_book_button = tk.Button(root, text="本の削除", command=show_delete_book)
-check_availability_button = tk.Button(root, text="貸出状況確認", command=show_check_availability)
-return_book_button = tk.Button(root, text="本の返却", command=show_return_book)
+# ボタンの配置
+list_books_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
+add_book_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
+search_book_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
+lend_book_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
+delete_book_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
+check_availability_button.pack(side=tk.LEFT, padx=5, pady=(20, 10), expand=True)
 
-list_books_button.pack()
-add_book_button.pack()
-search_book_button.pack()
-lend_book_button.pack()
-delete_book_button.pack()
-check_availability_button.pack()
-return_book_button.pack()
+# ウィンドウのリサイズイベントに対応するためのバインディング
+def on_resize(event):
+    center_window(root, event.width, event.height)
+
+root.bind("<Configure>", on_resize)
+
+# ウィンドウを中央に配置
+center_window(root, window_width, window_height)
 
 root.mainloop()
