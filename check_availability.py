e9f3c3cb697265728184916522580cde9b289fba
mujina <a.mujinasawa.sys22@morijyobi.ac.jp>
2023-11-15 14:59:29 +0900

    一覧と貸し出し状況確認の被りの機能削除

--- check_availability.py
+++ check_availability.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox
-from tkcalendar import DateEntry
 import psycopg2
-from datetime import datetime, timedelta
+from tkinter import messagebox
+from datetime import datetime
 import webbrowser
 
+# データベースの設定
 db_config = {
     'dbname': 'mydb',
     'user': 'user1',
@@ -12,59 +12,109 @@ db_config = {
     'host': 'localhost'
 }
 
-def calculate_days_elapsed(lend_date):
+def calculate_days_borrowed(lend_date):
     today = datetime.now().date()
-    days_elapsed = (today - lend_date).days
-    return days_elapsed
+    days_borrowed = (today - lend_date).days
+    return max(0, days_borrowed)
 
 def send_email(mail, book_title):
-
     email_url = f"https://mail.google.com/mail/?view=cm&fs=1&to={mail}&su=返却リマインダー&body={book_title}の返却期限が過ぎています。"
     webbrowser.open(email_url)
+
+def send_reminder(mail, book_title):
+    send_email(mail, book_title)
 
 def show_check_availability():
     check_availability_window = tk.Toplevel()
     check_availability_window.title("貸出状況確認")
 
     try:
-        
         conn = psycopg2.connect(**db_config)
         cursor = conn.cursor()
 
-        cursor.execute("SELECT id, title, mail, ISBN, lend_date FROM list")
-        lending_list = cursor.fetchall()
+        # 本とリストのテーブルを結合して、貸し出し中の本だけを取得するクエリ
+        cursor.execute("SELECT b.id, b.title, b.author, b.company, b.ISBN FROM book b JOIN list l ON b.ISBN = l.ISBN")
+        books = cursor.fetchall()
 
-        header_labels = ["ID", "本のタイトル", "メールアドレス", "ISBN", "貸出日", "貸出経過日数"]
-        for i, label_text in enumerate(header_labels):
-            header_label = tk.Label(check_availability_window, text=label_text)
-            header_label.grid(row=0, column=i, padx=5, pady=5)
+        frame = tk.Frame(check_availability_window)
+        frame.pack()
 
-        for row_num, lend_info in enumerate(lending_list, start=1):
-            id, title, mail, ISBN, lend_date = lend_info
-            days_elapsed = calculate_days_elapsed(lend_date)
+        id_label = tk.Label(frame, text="ID")
+        id_label.grid(row=0, column=0)
+        title_label = tk.Label(frame, text="本のタイトル")
+        title_label.grid(row=0, column=1)
+        author_label = tk.Label(frame, text="著者")
+        author_label.grid(row=0, column=2)
+        company_label = tk.Label(frame, text="出版社")
+        company_label.grid(row=0, column=3)
+        isbn_label = tk.Label(frame, text="ISBN")
+        isbn_label.grid(row=0, column=4)
+        days_borrowed_label = tk.Label(frame, text="貸出日数")
+        days_borrowed_label.grid(row=0, column=5)
+        mail_label = tk.Label(frame, text="メールアドレス")
+        mail_label.grid(row=0, column=6)
+        reminder_label = tk.Label(frame, text="メール送信")
+        reminder_label.grid(row=0, column=7)
 
-            days_elapsed_color = "red" if days_elapsed > 7 else "black"
-
-            labels = [id, title, mail, ISBN, lend_date.strftime('%Y-%m-%d'), days_elapsed]
-            for i, value in enumerate(labels):
-                label = tk.Label(check_availability_window, text=str(value), fg=days_elapsed_color)
-                label.grid(row=row_num, column=i, padx=5, pady=5)
-
-            if days_elapsed > 7:
-                send_email_button = tk.Button(check_availability_window, text="メール送信", command=lambda m=mail, b=title: send_email(m, b))
-                send_email_button.grid(row=row_num, column=len(header_labels), padx=5, pady=5)
+        row_number = 1
+        for book in books:
+            id_entry = tk.Entry(frame)
+            id_entry.insert(0, book[0])
+            id_entry.grid(row=row_number, column=0)
+            title_entry = tk.Entry(frame)
+            title_entry.insert(0, book[1])
+            title_entry.grid(row=row_number, column=1)
+            author_entry = tk.Entry(frame)
+            author_entry.insert(0, book[2])
+            author_entry.grid(row=row_number, column=2)
+            company_entry = tk.Entry(frame)
+            company_entry.insert(0, book[3])
+            company_entry.grid(row=row_number, column=3)
+            isbn_entry = tk.Entry(frame)
+            isbn_entry.insert(0, book[4])
+            isbn_entry.grid(row=row_number, column=4)
+            cursor.execute("SELECT COUNT(*) FROM list WHERE ISBN = %s", (book[4],))
+            is_lent = cursor.fetchone()[0]
+            if is_lent > 0:
+                cursor.execute("SELECT lend_date, mail FROM list WHERE ISBN = %s", (book[4],))
+                lend_info = cursor.fetchone()
+                lend_date = lend_info[0]
+                mail = lend_info[1]
+                days_borrowed = calculate_days_borrowed(lend_date)
+                mail_entry = tk.Entry(frame)
+                mail_entry.insert(0, mail)
+                mail_entry.grid(row=row_number, column=6)
+                days_borrowed_entry = tk.Entry(frame)
+                days_borrowed_entry.insert(0, days_borrowed)
+                days_borrowed_entry.grid(row=row_number, column=5)
+                if days_borrowed > 7:
+                    id_entry.config(bg="red")
+                    title_entry.config(bg="red")
+                    author_entry.config(bg="red")
+                    company_entry.config(bg="red")
+                    isbn_entry.config(bg="red")
+                    days_borrowed_entry.config(bg="red")
+                    mail_entry.config(bg="red")
+                    reminder_button = tk.Button(frame, text="メール送信", command=lambda m=mail, b=book[1]: send_reminder(m, b))
+                    reminder_button.grid(row=row_number, column=7)
+                else:
+                    id_entry.config(bg="lightgray")  # 7日以内の場合の色
+                    title_entry.config(bg="lightgray")
+                    author_entry.config(bg="lightgray")
+                    company_entry.config(bg="lightgray")
+                    isbn_entry.config(bg="lightgray")
+                    days_borrowed_entry.config(bg="lightgray")
+                    mail_entry.config(bg="lightgray")
+            row_number += 1
 
         cursor.close()
+        conn.close()
 
     except Exception as e:
-        tk.messagebox.showerror("Error", f"An error occurred: {str(e)}")
-
-    finally:
-        if conn:
-            conn.close()
+        messagebox.showerror("エラー", f"データベースから本の一覧を取得する際にエラーが発生しました: {str(e)}")
 
     close_button = tk.Button(check_availability_window, text="閉じる", command=check_availability_window.destroy)
-    close_button.grid(row=row_num + 1, column=0, columnspan=len(header_labels), pady=10)
+    close_button.pack()
 
 if __name__ == "__main__":
     show_check_availability()
